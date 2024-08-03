821b4102763176775abe78ba60634f9376530285
Musawer Hussain <musawerhussain1214@gmail.com>
2024-08-03 15:10:23 +0500

    Give choice of quiting

--- rdnm.py
+++ rdnm.py
@@ -3,12 +3,18 @@ import random
 target = random.randint(1, 100)
 
 while True:
-    user_Choice = int(input("Guess the number between 1 and 100 :"))
-    if (user_Choice == target):
-        print("You guess it right")
-        print(target)
-        
+    #First we will check if he want to continue the game or quit the game
+    user_Choice = input("Guess a number or press q to quit the game :")
+    if (user_Choice == "q"):
         break
+
+    user_Choice = int(user_Choice)
+    #In the above line we will convert the inout into an Integer to compare it with the guess number
+    if (user_Choice == target):
+        print("Congrats! You guess it right")
+        print(target)
+        break
+    
     elif(user_Choice > target):
         print("Your Choice is too large, take a smaller guess ")
     else:
