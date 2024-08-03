61d3d1324a734c988184ccca4baaae3aedddbca0
Musawer Hussain <musawerhussain1214@gmail.com>
2024-08-03 10:42:24 +0500

    Complete guess code

--- rdnm.py
+++ rdnm.py
@@ -1,5 +1,18 @@
 import random
 
-randNum = random.randint(1, 10)
+target = random.randint(1, 100)
 
-print (randNum)
+while True:
+    user_Choice = int(input("Guess the number between 1 and 100 :"))
+    if (user_Choice == target):
+        print("You guess it right")
+        print(target)
+        
+        break
+    elif(user_Choice > target):
+        print("Your Choice is too large, take a smaller guess ")
+    else:
+        print("Your Choice is too small, take a larger guess ")
+
+
+print("_____gameover____")
